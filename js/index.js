4be6867bc5dce21923f4d4d6eacf723a1f8c7e52
Diego <d9730707@gmail.com>
2021-01-13 00:20:04 -0500

    termine todoooo, hasta q encuentre un bug :C

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+  $('[data-toggle="tooltip"]').tooltip()
+})
+$(function(){
+  $('[data-toggle="tooltip-buttons"]').tooltip()
+});
+
+
+
+
+
+// Ejemplo de Js de inicio para deshabilitar el envío de formularios si hay campos no válidos
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
